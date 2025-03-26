0045a0c6d6e191f02e321abe6e87843aa195c283
MuhammadMouzzam <mouzzamalimughal2005@gmail.com>
2025-03-26 14:38:57 +0500

    changes to Lab_07 test bench: importing internal signal of RTL to bench

--- Lab_07/bench/seq_seven_seg_tb.sv
+++ Lab_07/bench/seq_seven_seg_tb.sv
@@ -1,14 +1,5 @@
 `timescale 1ns / 1ps
 
-/*
-Important Note: To prepare a self testing test bench, I had to include another output in the rtl module, new_sel,
-which I used to verify the output of the anode and cathode decoder functions after the write gets disabled. This
-was necessary because the new_sel output of the rtl module is not available in the test bench. But the rtl I submitted
-does not have this output and the reason is because if I did, I was required to bound the output to some output port
-on the FPGA during synthesis and implementation which was not asked for in the assignment. So, I had to remove the new_sel
-output from the rtl module before submission. If you intend to check it, then please add the new_sel output to the rtl module.
-*/
-
 module seq_seven_seg_tb();
     logic write;
     logic [3:0] num;
@@ -17,8 +8,6 @@ module seq_seven_seg_tb();
     logic reset;
     logic [7:0] anode;
     logic [6:0] cathodes;
-    logic [2:0] new_sel;
-    int num_memory [8] = {0,0,0,0,0,0,0,0};
 
     seq_seven_seg UUT(
         .write(write),
@@ -27,10 +16,10 @@ module seq_seven_seg_tb();
         .clk(clk),
         .reset(reset),
         .anode(anode),
-        .cathodes(cathodes),
-        .new_sel(new_sel)
+        .cathodes(cathodes)
     );
 
+int num_memory [8] = {0,0,0,0,0,0,0,0};
 
 //clk generator
 initial begin
@@ -48,14 +37,12 @@ task reseter;
     reset <= #1 0;
 endtask
 
-//Toggles the write signal when run
+
 task write_toggler;
     @(posedge clk);
     write <= #1 ~write;
 endtask
 
-//Assigns values to the num and sel signals(assigns random values if not provided)
-//and stores the values in the memory array for verifcation
 task driver(input logic [3:0] rnum = $random, input logic [2:0] rsel = $random);
     @(posedge clk);
     num_memory[rsel] = rnum;
@@ -64,7 +51,6 @@ task driver(input logic [3:0] rnum = $random, input logic [2:0] rsel = $random);
     @(posedge clk);
 endtask
 
-//decodes the anode and cathode signals
 function [7:0] anode_decoder(input logic [2:0] rsel);
     logic [7:0] anode_out;
     case(rsel)
@@ -102,32 +88,22 @@ function [6:0] cathode_decoder(input logic [3:0] rnum);
         endcase
 endfunction
 
-//monitors the outputs of the RTL module
 task monitor();
     logic [7:0] expected_anode;
     logic [6:0] expected_cathode;
     @(posedge clk)
-    expected_anode = anode_decoder(new_sel);
-    expected_cathode = cathode_decoder(num_memory[new_sel]);
+    expected_anode = anode_decoder(UUT.new_sel);
+    expected_cathode = cathode_decoder(num_memory[UUT.new_sel]);
 
     if(expected_anode != anode)
-        $display("Error-In-Anode: Number = %d, selector = %d, Expected Anode Output = %b, Got = %b", num_memory[new_sel], new_sel, expected_anode, anode);
+        $display("Error-In-Anode: Number = %d, selector = %d, Expected Anode Output = %b, Got = %b", num_memory[UUT.new_sel], UUT.new_sel, expected_anode, anode);
     else
-        $display("Anode-Pass: Number = %d, Selector = %d, Got = %b", num_memory[new_sel], new_sel, anode);
+        $display("Anode-Pass: Number = %d, Selector = %d, Got = %b", num_memory[UUT.new_sel], UUT.new_sel, anode);
     if(expected_cathode != cathodes)
-        $display("Error-In-Cathode: Number = %d, selector = %d, Expected Cathode Output = %b, Got = %b", num_memory[new_sel], new_sel, expected_cathode, cathodes);
+        $display("Error-In-Cathode: Number = %d, selector = %d, Expected Cathode Output = %b, Got = %b", num_memory[UUT.new_sel], UUT.new_sel, expected_cathode, cathodes);
     else
-        $display("Cathode-Pass: Number = %d, Selector = %d, Got = %b", num_memory[new_sel], new_sel, cathodes);
+        $display("Cathode-Pass: Number = %d, Selector = %d, Got = %b", num_memory[UUT.new_sel], UUT.new_sel, cathodes);
 endtask
-
-//Initialization of the test bench
-initial begin
-    write_toggler;
-end
-
-initial begin
-    reseter;
-end
 
 initial begin
     for (int i = 0; i < 8; i++) begin
@@ -138,6 +114,12 @@ initial begin
         monitor();
     end
 end
+initial begin
+    write_toggler;
+end
 
+initial begin
+    reseter;
+end
 
 endmodule
